e56a1f1024acec1150a96a3e0667f8b24387d7ed
Zaid Al Lahham <zlahham@gmail.com>
2015-09-21 18:29:31 +0100

    Adding forms for the event page

--- spec/features/events_feature_spec.rb
+++ spec/features/events_feature_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
-# describe 'Events' do
-#   it 'can be created by a user' do
-#     visit root_path
-#     sign_up_user
-#     click_on 'Create Event'
-#   end
-# end
+describe 'Events' do
+  it 'can be created by a user with one question' do
+    visit root_path
+    sign_up_user
+    click_on 'Create Event'
+    
+    fill_in 'Event name', with: 'event 1'
+    click_on 'Add question'
+    fill_in 'Question', with: 'Ruby vs Javascript'
+    fill_in 'Choice 1', with: 'Ruby'
+    fill_in 'Choice 2', with: 'Javascript'
+    click_on 'Publish'
+    expect(page).to have_content 'Your event has been published'
+    # expect(page).to have_content 'LINK TO EVENT'
+  end
+
+  def sign_up_user
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'Email', with: "user@email.com"
+    fill_in 'Password', with: "password"
+    fill_in 'Password confirmation', with: "password"
+    click_on 'Sign up'
+  end
+end
