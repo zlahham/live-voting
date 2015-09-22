9da74484ba552c6e8fefc0074337c358be67e016
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-22 10:51:34 +0100

    split test into two

--- spec/features/events_feature_spec.rb
+++ spec/features/events_feature_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 describe 'Events', js: true do
-  it 'can be created by a user with one question' do
+  it 'can be created' do
     visit root_path
     sign_up_user
     click_on 'Create Event'
     
     fill_in 'event_title', with: 'event 1'
     click_on 'Add question'
-    fill_in 'Question', with: 'Ruby vs Javascript'
-    fill_in 'Choice 1', with: 'Ruby'
-    fill_in 'Choice 2', with: 'Javascript'
-    click_on 'Publish'
-    expect(page).to have_content 'Your event has been published'
-    expect(page).to have_content 'LINK TO EVENT'
+    expect(page).to have_content 'event 1'
   end
+
+  # it 'can be created by a user with one question' do
+
+  #   fill_in 'Question', with: 'Ruby vs Javascript'
+  #   fill_in 'Choice 1', with: 'Ruby'
+  #   fill_in 'Choice 2', with: 'Javascript'
+  #   click_on 'Publish'
+  #   expect(page).to have_content 'Your event has been published'
+  #   expect(page).to have_content 'LINK TO EVENT'
+  # end
 
   def sign_up_user
     visit root_path
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can be created' do
+    expect{Event.create}.to change{Event.count}.by(1)
+  end
 end
