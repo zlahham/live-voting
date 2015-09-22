a35f735a5bf2f7d9f42b0394c4824f2049ec4953
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-22 16:56:12 +0100

    question validates presence of event

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :event
+
+  validates :event, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it{ is_expected.to belong_to :event }
+  it{ is_expected.to validate_presence_of :event }
 end
