b34dd2467a19af86d132eff9fbce127908de6362
Ben <ben.c.hawker@gmail.com>
2015-09-23 12:45:42 +0100

    Add Votes to Choices

--- spec/models/choice_spec.rb
+++ spec/models/choice_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Choice, type: :model do
   it { is_expected.to belong_to :question }
+  it { is_expected.to have_many :votes }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   it { is_expected.to belong_to :choice }
+  it { is_expected.to validate_presence_of :choice }
+
+  it "can be created on a choice" do
+  	choice = create(:choice)
+  	expect{choice.votes.create}.to change{choice.votes.count}.by 1
+  end
+
 end
