9e51ebab75738de71d60b653fb2a857ff18b6ed6
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-24 17:00:17 +0100

    build json test

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe QuestionsController do
 
   describe '#publish_question' do
     it 'fires pusher' do
-      user = create(:user)
-      event = user.events.create(title: 'test')
-      question = event.questions.create(content: 'test question')
-      choice = question.choices.create(content: 'test choice')
-      choice2 = question.choices.create(content: 'best choice')
-      sign_in_as(user)
+      # user = create(:user)
+      # event = user.events.create(title: 'test')
+      # question = event.questions.create(content: 'test question')
+      # choice = question.choices.create(content: 'test choice')
+      # choice2 = question.choices.create(content: 'best choice')
+      # sign_in_as(user)
 
-      visit event_path(event)
-      click_on 'Publish'
+      # visit event_path(event)
+      # click_on 'Publish'
 
 
 
@@ -29,7 +29,25 @@ RSpec.describe QuestionsController do
     end
   end
 
-    def sign_in_as(user)
+  describe '#build_json' do
+    it 'returns an acceptable json object for pusher' do
+      user =      create(:user)
+      event =     user.events.create(title: 'test')
+      question =  event.questions.create(content: 'test question')
+      choice =    question.choices.create(content: 'test choice')
+      choice2 =   question.choices.create(content: 'best choice')
+      
+      sign_in_as(user)
+
+      p = QuestionsController.new
+
+      expect(JSON.parse(p.build_json event, question)).to eq( {"event"=>{"id"=>event.id, "title"=>"test"}, "question"=>{"id"=>question.id, "content"=>"test question"}, "choices"=>[{"content"=>"test choice", "id"=>choice.id}, {"content"=>"best choice", "id"=>choice2.id}]} )
+    end
+  end
+
+  private
+
+  def sign_in_as(user)
     visit root_path
     click_link 'Sign in'
     fill_in :user_email, with: user.email
