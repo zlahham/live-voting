8fe129e8dbb99a96bf22dab3a7c97a6fba548497
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-24 19:31:38 +0100

    refactor a little

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController do
-  include Capybara::DSL
 
   describe '#publish_question' do
     it 'calls push_json_to_pusher method' do
@@ -23,21 +22,9 @@ RSpec.describe QuestionsController do
       choice =    question.choices.create(content: 'test choice')
       choice2 =   question.choices.create(content: 'best choice')
       
-      sign_in_as(user)
-
       p = QuestionsController.new
 
       expect(JSON.parse(p.build_json event, question)).to eq( {"event"=>{"id"=>event.id, "title"=>"test"}, "question"=>{"id"=>question.id, "content"=>"test question"}, "choices"=>[{"content"=>"test choice", "id"=>choice.id}, {"content"=>"best choice", "id"=>choice2.id}]} )
     end
-  end
-
-  private
-
-  def sign_in_as(user)
-    visit root_path
-    click_link 'Sign in'
-    fill_in :user_email, with: user.email
-    fill_in :user_password, with: user.password
-    click_button 'Log in'
   end
 end
