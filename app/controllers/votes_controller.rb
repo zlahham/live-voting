a167083324fbd6f1bfdc93b3c09f9114a1e63180
lewis <ljones140@gmail.com>
2015-09-25 14:50:37 +0100

    ajax is working

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,7 @@
 class VotesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def create
     @choice = Choice.find(params[:choice])
     @choice.votes.create
