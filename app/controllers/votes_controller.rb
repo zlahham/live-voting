5d1b0aa024197371583a76ef648071edc4cc77ff
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-25 17:12:30 +0100

    build json object and send to pusher

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,25 @@
 class VotesController < ApplicationController
-
   skip_before_action :verify_authenticity_token
 
   def create
-    @choice = Choice.find(params[:choice])
-    @choice.votes.create
+    choice = Choice.find(params[:choice])
+    vote = choice.votes.new
+    
+    if vote.save
+      json_object = build_json(vote)
+      push_json(choice, json_object)
+    end
+
     render nothing: true
+  end
+
+  def build_json(vote)
+    json_object = { choice_id: vote.choice.id, vote_count: vote.choice.votes.count }.to_json
+  end
+
+  def push_json(choice, json_object)
+    pusher = Pusher::Client.new app_id: Pusher.app_id, key: Pusher.key, secret: Pusher.secret
+    pusher.trigger('test_channel', 'event_question_' +
+     choice.question_id.to_s + '_' + 'choice_' + choice.id.to_s, json_object )
   end
 end
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe VotesController, type: :controller do
       expect{get(:create)}.to change{choice.votes.count}.by(1)
     end
   end
+
+  describe '#build_json' do
+    it "when passed a vote, it returns a json object with choice_id and that choice's vote count" do
+      choice = create(:choice)
+      vote = create(:vote, choice: choice)
+
+      expect(JSON.parse(subject.build_json(vote))).to eq( {"choice_id" => choice.id, "vote_count" => vote.choice.votes.count} )
+    end
+  end
 end
