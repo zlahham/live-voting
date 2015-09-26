ac8e86dbab28771204f99a51c452d66fbd0fcef9
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-26 02:06:13 +0100

    minor refactoring tweak on questions features spec

--- spec/features/questions_feature_spec.rb
+++ spec/features/questions_feature_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe 'Questions Features' do
+  let(:event){ create :event, user: create(:user) }
+
   before :each do
-    user = create :user
-    sign_in_as(user)
-    @event = create :event, user: user
+    sign_in_as(event.user)
   end
 
   it 'can be created on an event' do
-    visit event_path(@event)
+    visit event_path(event)
     click_on 'Add Question'
     fill_in 'question_content', with: 'test question'
     click_on 'Add'
@@ -17,29 +17,29 @@ describe 'Questions Features' do
   end
 
   it "can't be created with a blank content field" do
-    visit event_path(@event)
+    visit event_path(event)
     click_on 'Add Question'
     click_on 'Add'
     expect(page).to have_content "1 error prohibited this question from being saved:"
   end
 
   context 'after a question has been created' do
-    before(:each){ @question = create :question, event: @event }
+    before(:each){ @question = create :question, event: event }
 
     it "the question show page has a link back to its event" do
       visit question_path(@question)
-      expect(page).to have_selector(:link_or_button, "Back to #{@event.title}")
+      expect(page).to have_selector(:link_or_button, "Back to #{event.title}")
     end
 
     it "event show page has a button to 'Publish' the question" do
-      visit event_path(@event)
+      visit event_path(event)
       expect(page).to have_selector(:link_or_button, 'Publish')
     end
 
     it "user can publish the question" do
       #this expect statement stubs the trigger method
       expect_any_instance_of(Pusher::Client).to receive(:trigger)
-      visit event_path(@event)
+      visit event_path(event)
       click_on 'Publish'
       expect(page).to have_content 'Question has been pushed to the audience'
     end
