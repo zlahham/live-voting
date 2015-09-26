331bf97715a4bf93f85cd8c8d4464f92ec967836
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-26 18:42:18 +0100

    events are destroyed when their parent users are destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :events
-
+  has_many :events, dependent: :destroy
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  let(:user){ create :user }
+
 
   it { is_expected.to validate_presence_of :user }
   it { is_expected.to validate_presence_of :title }
@@ -8,6 +10,13 @@ RSpec.describe Event, type: :model do
   it { is_expected.to have_many :questions }
   it { is_expected.to have_many :voters }
 
+
+  it 'is destroyed when parent user is destroyed' do
+    event = create :event, user: user
+
+    expect{ user.destroy }.to change{ Event.count }.by -1
+    expect( user.events.include? event ).to eq false
+  end
 
   describe 'create event' do
     it 'can be created' do
