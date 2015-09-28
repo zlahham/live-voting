89d1d86c3021269eef6c65d8a83f2634ceebb3b5
lewis <ljones140@gmail.com>
2015-09-28 11:36:22 +0100

    test created with factory

--- spec/factories/choices.rb
+++ spec/factories/choices.rb
@@ -1,5 +1,14 @@
 FactoryGirl.define do
   factory :choice do
     content "Yes"
+
+    factory :choice_2 do
+      content "No"
+    end
+
+    factory :choice_3 do
+      content "Maybe"
+    end
+
   end
 end
--- spec/features/voting_feature_spec.rb
+++ spec/features/voting_feature_spec.rb
@@ -1,47 +1,58 @@
 require 'rails_helper'
 
 feature 'Voting Features' do
-  let(:event){ create :event, user: create(:user) }
+  # let(:event){ create :event, user: create(:user) }
 
   context 'when not signed in' do
+
+    before(:each) do
+      @event = create :event, user: create(:user)
+      @question = create :question, event: @event
+      @choice = create :choice, question: @question
+      @choice2 = create :choice_2, question: @question
+      @choice3 = create :choice_3, question: @question
+      @data = {
+        event: {
+          id: @event.id,
+          title: @event.title
+        },
+        question: {
+          id: @question.id,
+          content: @question.content
+        },
+        choices: [
+          {
+            content: @choice.content,
+            id: @choice.id
+          },
+          {
+            content: @choice2.content,
+            id: @choice2.id
+          },
+          {
+            content: @choice3.content,
+            id: @choice3.id
+          }
+        ]
+      }
+    end
+
     it "can navigate to voting page and it says 'Awaiting Question'" do
-      visit vote_event_path(event)
+      visit vote_event_path(@event)
       expect(page).to have_content 'Awaiting Question'
     end
 
-    data = {
-      event: {
-        id: 8,
-        title: "Test Question"
-      },
-      question: {
-        id: 10,
-        content: "Test Question"
-      },
-      choices: [
-        {
-          content: "yes",
-          id: 15
-        },
-        {
-          content: "no",
-          id: 16
-        },
-        {
-          content: "maybe",
-          id: 17
-        }
-      ]
-    }
+
 
     it "sends question to voter", js: :true do
-      visit vote_event_path(event)
+      visit vote_event_path(@event)
       expect(page).to have_content 'Awaiting Question'
-      page.execute_script("$(document).ready(function() { buildQuestion(#{data.to_json}) });")
-      expect(page).to have_content 'Test Question'
-      expect(page).to have_content 'yes'
-      expect(page).to have_content 'no'
-      expect(page).to have_content 'maybe'
+      page.execute_script("$(document).ready(function() { buildQuestion(#{@data.to_json}) });")
+      expect(page).to have_content 'Question 1 of 1' 
+      expect(page).to have_content 'My Question'
+      expect(page).to have_content 'Yes'
+      expect(page).to have_content 'No'
+      expect(page).to have_content 'Maybe'
     end
   end
 end
