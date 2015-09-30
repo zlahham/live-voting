ed079894ac9636fba48b1df0c0598300eff26320
lewis <ljones140@gmail.com>
2015-09-30 11:58:48 +0100

    246-add-multicolours-charts

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,4 +1,5 @@
 module QuestionHelper
+
   COLOUR_ARRAY = [
     'progress-bar progress-bar-success',
     'progress-bar progress-bar-info',
@@ -8,11 +9,9 @@ module QuestionHelper
 
   def any_votes?(question)
     votes = false
-
     question.choices.each do |choice|
       votes = true if choice.votes.any?
     end
-
     votes
   end
 
