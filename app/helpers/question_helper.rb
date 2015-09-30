6faf8d80f9b46450db983240ba1d1fd2246fb94e
Zaid Al Lahham <zlahham@gmail.com>
2015-09-30 11:42:40 +0100

    Colours are now rotated but the same colour dont persist when refreshed

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,6 +1,12 @@
 module QuestionHelper
+  COLOUR_ARRAY = [
+    'progress-bar progress-bar-success',
+    'progress-bar progress-bar-info',
+    'progress-bar progress-bar-warning',
+    'progress-bar progress-bar-danger'
+  ]
 
-  def any_votes? question
+  def any_votes?(question)
     votes = false
 
     question.choices.each do |choice|
@@ -10,11 +16,16 @@ module QuestionHelper
     votes
   end
 
-  def total_votes question
+  def total_votes(question)
     vote_count = 0
     question.choices.each do |choice|
       vote_count += choice.votes.count
     end
-    vote_count 
+    vote_count
+  end
+
+  def graph_colour
+    COLOUR_ARRAY.rotate!
+    COLOUR_ARRAY[0]
   end
 end
